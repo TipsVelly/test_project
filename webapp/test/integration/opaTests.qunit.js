56ca87eeeb2ea2118bc87ddadabbeb47d08a21e0
yesman5307 <yesman5307@gmail.com>
2024-09-24 14:08:45 +0900

    commit:init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/tipsvally/testproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comtipsvally/test_project/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
